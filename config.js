f767929b3efbc5671b732f25122fdc9503759f3e
Shashank Yadav <42097794+yshashanky@users.noreply.github.com>
2020-05-30 01:09:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Resume', // <title>
+    siteTitle: 'Shashank Yadav', // <title>
   manifestName: 'Resume',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
